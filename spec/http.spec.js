c1a00403508aaf7fdceddfb6a77e0224b13eeb63
Bakhtiyari <h.galeh@gmail.com>
2020-08-03 08:44:58 +0430

    http

---
+++ spec/http.spec.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const { hasUncaughtExceptionCaptureCallback } = require('process');
+
+describe('http', () => {
+    it('return hello', (done) => {
+        http.createServer((req, res) => {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write('hello');
+            res.end();
+        }).listen(8080);
+        const options = {
+            host: 'localhost',
+            port: 8080
+        };
+        http.request(options, (response) => {
+            let str = '';
+            response.on('data', (chunk) => {
+                str += chunk;
+            });
+            response.on('end', () => {
+                expect(str).toBe('hello');
+                done()
+            })
+        }).end();        
+    });
+});
